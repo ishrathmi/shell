59f7e08d58d111692e2988a10538599cc6c3592f
Ishrath Irshadeen <ishrathirshadeen@ishraths-mbp.home>
2024-08-21 22:33:40 -0400

    check these changes again

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -14,7 +14,8 @@ mkdir dir1 dir2 dir3 dir4 dir5
 ls
 
 # 3. Create 5 text files in dir2 named file1, file2, file3, file4, and file5
-touch dir2/file1 dir2/file2 dir2/file3 dir2/file4 dir2/file5
+cd dir2
+touch file1 file2 file3 file4 file5
 
 # 4. Append the words "Hello world" to dir2/file3
 echo 'Hello world' > dir2/file3
@@ -29,4 +30,5 @@ rm dir2/file4
 rm -r -i dir4/* dir5/*
 
 # 8. List the contents of the parent directory to verify the deletion of dir4 and dir5
+cd
 ls 
